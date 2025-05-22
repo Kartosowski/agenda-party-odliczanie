4a89cd87406709c1ad6653c36dd855c41fdf691a
Alex Tuncer <71525603+Kartosowski@users.noreply.github.com>
2025-05-23 00:28:24 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
+
 // app/page.tsx
+'use client'
 import Laptop from '../app/../../components/Laptop'
 
 export default function Home() {
   return (
-    <div>
+    <div> 
       <div className='absolute bottom-0 p-2 text-sm text-white text-shadow-black text-shadow-lg'>
         <p>Odliczanie do Agenda Party</p>
         <p>Wykonawca: Kartos (Alex Tuncer)</p>
         <p>Model wykonany przez daniyalmalik na Sketchfab</p>
+        <p>Projekt: https://github.com/Kartosowski/agenda-party-odliczanie</p>
+        <p>Jest szansa, że na IPhonie nie działa w 100%</p>
       </div>
       <Laptop />
-
     </div> 
     )
 }
